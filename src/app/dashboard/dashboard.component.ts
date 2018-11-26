3c7d27667c0c328095ab5ec3006960dd28d07aaa
Alexandre Consigny <alexandre.consigny@gmail.com>
2018-11-26 01:23:43 -0500

    Push commit to master

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,7 +17,7 @@ export class DashboardComponent implements OnInit {
   }
 
   getHeroes(): void {
-    //
+    //Add
     this.heroService
       .getHeroes()
       .subscribe(heroes => (this.heroes = heroes.slice(1, 5)));
